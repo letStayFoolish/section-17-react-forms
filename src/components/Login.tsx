bc18f9a4cdcd58366a17eaf6690b25502ce70060
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-11-24 08:22:48 +0100

    feat: Add error handling for place updates and improve Login form
    Enhanced the App component to handle errors during place updates by setting user places and displaying an error modal. Refactored the Login component to manage form data with a single state object, simplifying input handling and submission.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,73 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+
+type TFormValues = {
+  email: string;
+  password: string;
+};
 
 const Login: React.FC = () => {
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState<TFormValues>({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(
+      "User Email: " + enteredValues.email,
+      "Password: " + enteredValues.password,
+    );
+  };
+
+  // const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setEmail(event.target.value);
+  // };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredValues((prevValues) => {
+      return {
+        ...prevValues,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="submit" className="button">
+          Login
+        </button>
       </p>
     </form>
   );
